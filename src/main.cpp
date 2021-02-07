f8ce5e9a1c62a439d3d4eb972ce777081dfa6739
AlexCr4ckPentest <alexandr-119@mail.ru>
2021-02-07 12:52:59 +0300

    add subdirectory src/ and main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include "../include/overload.hpp"
+#include "../include/universal_function_pointer.hpp"
+#include "../include/serializable_deserializable.hpp"
+
+
+
+int main()
+{
+    return 0;
+}
