fd9746120160f9960a77784b3792959d44810965
AlexCr4ckPentest <alexandr-119@mail.ru>
2021-02-09 15:19:21 +0300

    add utils.hpp

---
+++ include/utils/utils.hpp
@@ -0,0 +1,11 @@
+#ifndef UTILS_HPP
+#define UTILS_HPP
+
+#include "overload.hpp"
+#include "universal_function_pointer.hpp"
+#include "transparent_invoke.hpp"
+#include "memory_location_check.hpp"
+
+#include "smart_pointers/smart_pointers.hpp"
+
+#endif // UTILS_HPP
