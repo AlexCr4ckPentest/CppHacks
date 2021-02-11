a826c413800d6e80badec1eaa28cff9efeaf7b38
AlexCr4ckPentest <alexandr-119@mail.ru>
2021-02-11 16:52:12 +0300

    update utils module header

--- include/utils/utils.hpp
+++ include/utils/utils.hpp
@@ -5,7 +5,10 @@
 #include "universal_function_pointer.hpp"
 #include "transparent_invoke.hpp"
 #include "memory_location_check.hpp"
+#include "concepts.hpp"
 
-#include "smart_pointers/smart_pointers.hpp"
+#include "math/math.hpp" // math module
+
+#include "smart_pointers/smart_pointers.hpp" // smart pointers module
 
 #endif // UTILS_HPP
