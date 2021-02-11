a4136e09c6b81bf6840187e037116ef3f639ef42
AlexCr4ckPentest <alexandr-119@mail.ru>
2021-02-11 23:12:12 +0300

    reworked ct_factorial and add concept for template argument

--- include/utils/math/compile_time_factorial.hpp
+++ include/utils/math/compile_time_factorial.hpp
@@ -13,114 +13,66 @@
 #ifndef COMPILE_TIME_FACTORIAL_HPP
 #define COMPILE_TIME_FACTORIAL_HPP
 
+#ifdef __RECURSIVE_TEMPLATE_INSTANTIATION_FACTORIAL_COMPUTING
 #warning "Be careful when using large numbers!"
 #warning "Add the -ftemplate-depth=N flag to increase the depth of template instantiation!"
-
-
-
-#define __DECLARE_CT_FACTORIAL_ON_STRUCTS_WITH_CUSTOM_TYPE_FRON_NAMESAPCE__(namespace_, integer_type)  \
-template<size_t N>  \
-struct ct_factorial_##integer_type  \
-{ static constexpr namespace_::integer_type value{N * ct_factorial_##integer_type<N - 1>::value}; };  \
-template<>  \
-struct ct_factorial_##integer_type<0> \
-{ static constexpr namespace_::integer_type value{1}; }
-
-
-
-#define __DECLARE_CT_FACTORIAL_ON_STRUCTS_WITH_NATIVE_TYPE__(integer_type)  \
-template<size_t N>  \
-struct ct_factorial_##integer_type  \
-{ static constexpr integer_type value{static_cast<integer_type>(N) * ct_factorial_##integer_type<N - 1>::value}; };  \
-template<>  \
-struct ct_factorial_##integer_type<0> \
-{ static constexpr integer_type value{1}; }
+#endif // __RECURSIVE_TEMPLATE_INSTANTIATION_FACTORIAL_COMPUTING
 
 
 
 namespace alex::utils::math::ct_factorial {
-namespace int_types {
+namespace concepts {
 
-// Signed int types
-using boost::multiprecision::int128_t;
-using boost::multiprecision::int256_t;
-using boost::multiprecision::int512_t;
-using boost::multiprecision::int1024_t;
+template<typename T>
+concept IntegerType = requires
+{ requires std::is_integral_v<T> || boost::multiprecision::is_number<T>::value; };
 
-// Unsigned int types
-using boost::multiprecision::uint128_t;
-using boost::multiprecision::uint256_t;
-using boost::multiprecision::uint512_t;
-using boost::multiprecision::uint1024_t;
+} // namespace alex::utils::math::ct_factorial::concepts
 
-// Safety signed safe int types
-using boost::multiprecision::checked_int128_t;
-using boost::multiprecision::checked_int256_t;
-using boost::multiprecision::checked_int512_t;
-using boost::multiprecision::checked_int1024_t;
+namespace detail {
 
-// Safety unsigned int types
-using boost::multiprecision::checked_uint128_t;
-using boost::multiprecision::checked_uint256_t;
-using boost::multiprecision::checked_uint512_t;
-using boost::multiprecision::checked_uint1024_t;
-} // namespace alex::utils::math::ct_factorial::int_types
+template<size_t N>
+constexpr size_t cached_factorial_value {
+    [] {
+        if constexpr (N <= 1)       return 1;
+        else if constexpr (N == 2)  return 2;
+        else if constexpr (N == 3)  return 6;
+        else if constexpr (N == 4)  return 24;
+        else if constexpr (N == 5)  return 125;
+    }()
+};
+
+} // namespace detail
 
 
 
-__DECLARE_CT_FACTORIAL_ON_STRUCTS_WITH_NATIVE_TYPE__(int8_t);
-__DECLARE_CT_FACTORIAL_ON_STRUCTS_WITH_NATIVE_TYPE__(int16_t);
-__DECLARE_CT_FACTORIAL_ON_STRUCTS_WITH_NATIVE_TYPE__(int32_t);
-__DECLARE_CT_FACTORIAL_ON_STRUCTS_WITH_NATIVE_TYPE__(int64_t);
-__DECLARE_CT_FACTORIAL_ON_STRUCTS_WITH_NATIVE_TYPE__(intmax_t);
+#ifdef __RECURSIVE_TEMPLATE_INSTANTIATION_FACTORIAL_COMPUTING
+template<concepts::IntegerType integer_type, size_t N>
+struct ct_factorial { static constexpr integer_type value{N * ct_factorial<integer_type, N - 1>::value}; };
 
-__DECLARE_CT_FACTORIAL_ON_STRUCTS_WITH_NATIVE_TYPE__(uint8_t);
-__DECLARE_CT_FACTORIAL_ON_STRUCTS_WITH_NATIVE_TYPE__(uint16_t);
-__DECLARE_CT_FACTORIAL_ON_STRUCTS_WITH_NATIVE_TYPE__(uint32_t);
-__DECLARE_CT_FACTORIAL_ON_STRUCTS_WITH_NATIVE_TYPE__(uint64_t);
-__DECLARE_CT_FACTORIAL_ON_STRUCTS_WITH_NATIVE_TYPE__(uintmax_t);
-
-__DECLARE_CT_FACTORIAL_ON_STRUCTS_WITH_NATIVE_TYPE__(int_fast8_t);
-__DECLARE_CT_FACTORIAL_ON_STRUCTS_WITH_NATIVE_TYPE__(int_fast16_t);
-__DECLARE_CT_FACTORIAL_ON_STRUCTS_WITH_NATIVE_TYPE__(int_fast32_t);
-__DECLARE_CT_FACTORIAL_ON_STRUCTS_WITH_NATIVE_TYPE__(int_fast64_t);
-
-__DECLARE_CT_FACTORIAL_ON_STRUCTS_WITH_NATIVE_TYPE__(int_least8_t);
-__DECLARE_CT_FACTORIAL_ON_STRUCTS_WITH_NATIVE_TYPE__(int_least16_t);
-__DECLARE_CT_FACTORIAL_ON_STRUCTS_WITH_NATIVE_TYPE__(int_least32_t);
-__DECLARE_CT_FACTORIAL_ON_STRUCTS_WITH_NATIVE_TYPE__(int_least64_t);
-
-__DECLARE_CT_FACTORIAL_ON_STRUCTS_WITH_NATIVE_TYPE__(uint_fast8_t);
-__DECLARE_CT_FACTORIAL_ON_STRUCTS_WITH_NATIVE_TYPE__(uint_fast16_t);
-__DECLARE_CT_FACTORIAL_ON_STRUCTS_WITH_NATIVE_TYPE__(uint_fast32_t);
-__DECLARE_CT_FACTORIAL_ON_STRUCTS_WITH_NATIVE_TYPE__(uint_fast64_t);
-
-__DECLARE_CT_FACTORIAL_ON_STRUCTS_WITH_NATIVE_TYPE__(uint_least8_t);
-__DECLARE_CT_FACTORIAL_ON_STRUCTS_WITH_NATIVE_TYPE__(uint_least16_t);
-__DECLARE_CT_FACTORIAL_ON_STRUCTS_WITH_NATIVE_TYPE__(uint_least32_t);
-__DECLARE_CT_FACTORIAL_ON_STRUCTS_WITH_NATIVE_TYPE__(uint_least64_t);
-
-
-
-__DECLARE_CT_FACTORIAL_ON_STRUCTS_WITH_CUSTOM_TYPE_FRON_NAMESAPCE__(int_types, int128_t);
-__DECLARE_CT_FACTORIAL_ON_STRUCTS_WITH_CUSTOM_TYPE_FRON_NAMESAPCE__(int_types, int256_t);
-__DECLARE_CT_FACTORIAL_ON_STRUCTS_WITH_CUSTOM_TYPE_FRON_NAMESAPCE__(int_types, int512_t);
-__DECLARE_CT_FACTORIAL_ON_STRUCTS_WITH_CUSTOM_TYPE_FRON_NAMESAPCE__(int_types, int1024_t);
-
-__DECLARE_CT_FACTORIAL_ON_STRUCTS_WITH_CUSTOM_TYPE_FRON_NAMESAPCE__(int_types, uint128_t);
-__DECLARE_CT_FACTORIAL_ON_STRUCTS_WITH_CUSTOM_TYPE_FRON_NAMESAPCE__(int_types, uint256_t);
-__DECLARE_CT_FACTORIAL_ON_STRUCTS_WITH_CUSTOM_TYPE_FRON_NAMESAPCE__(int_types, uint512_t);
-__DECLARE_CT_FACTORIAL_ON_STRUCTS_WITH_CUSTOM_TYPE_FRON_NAMESAPCE__(int_types, uint1024_t);
-
-__DECLARE_CT_FACTORIAL_ON_STRUCTS_WITH_CUSTOM_TYPE_FRON_NAMESAPCE__(int_types, checked_int128_t);
-__DECLARE_CT_FACTORIAL_ON_STRUCTS_WITH_CUSTOM_TYPE_FRON_NAMESAPCE__(int_types, checked_int256_t);
-__DECLARE_CT_FACTORIAL_ON_STRUCTS_WITH_CUSTOM_TYPE_FRON_NAMESAPCE__(int_types, checked_int512_t);
-__DECLARE_CT_FACTORIAL_ON_STRUCTS_WITH_CUSTOM_TYPE_FRON_NAMESAPCE__(int_types, checked_int1024_t);
-
-__DECLARE_CT_FACTORIAL_ON_STRUCTS_WITH_CUSTOM_TYPE_FRON_NAMESAPCE__(int_types, checked_uint128_t);
-__DECLARE_CT_FACTORIAL_ON_STRUCTS_WITH_CUSTOM_TYPE_FRON_NAMESAPCE__(int_types, checked_uint256_t);
-__DECLARE_CT_FACTORIAL_ON_STRUCTS_WITH_CUSTOM_TYPE_FRON_NAMESAPCE__(int_types, checked_uint512_t);
-__DECLARE_CT_FACTORIAL_ON_STRUCTS_WITH_CUSTOM_TYPE_FRON_NAMESAPCE__(int_types, checked_uint1024_t);
+template<concepts::IntegerType integer_type>
+struct ct_factorial<integer_type, 0> { static constexpr integer_type value{1}; };
+#elif defined(__RECURSIVE_CONSTEXPR_FUNCTION_FACTORIAL_COMPUTING)
+template<concepts::IntegerType integer_type, size_t N>
+constexpr auto ct_factorial()
+{
+    if constexpr (N <= 5) return detail::cached_factorial_value<N>;
+    else return N * ct_factorial<N - 1>();
+}
+#elif defined(__CONSTEXPR_FUNCTION_FOR_LOOP_FACTORIAL_COMPUTING)
+template<concepts::IntegerType integer_type, size_t N>
+constexpr auto ct_factorial()
+{
+    if constexpr (N <= 5) return detail::cached_factorial_value<N>;
+    else {
+        integer_type result{1};
+        for (size_t i{1}; i < N; ++i) {
+            result *= i;
+        }
+        return result;
+    }
+}
+#endif
 
 } // namespace alex::utils::math::ct_factorial
 
--- include/utils/math/math.hpp
+++ include/utils/math/math.hpp
@@ -8,6 +8,8 @@
 #ifndef MATH_HPP
 #define MATH_HPP
 
+#define __RECURSIVE_TEMPLATE_INSTANTIATION_FACTORIAL_COMPUTING // By default until new version
 #include "compile_time_factorial.hpp"
+#undef __RECURSIVE_TEMPLATE_INSTANTIATION_FACTORIAL_COMPUTING
 
 #endif // MATH_HPP
