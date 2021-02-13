37e1e97ad3407a45cbc7a5e3db3b23f7f31da990
AlexCr4ckPentest <alexandr-119@mail.ru>
2021-02-13 23:24:56 +0300

    add legacy SFINAE

--- include/utils/math/compile_time_power.hpp
+++ include/utils/math/compile_time_power.hpp
@@ -9,7 +9,6 @@
 #include <boost/multiprecision/cpp_int.hpp>
 
 
-
 #ifndef COMPILE_TIME_POWER_HPP
 #define COMPILE_TIME_POWER_HPP
 
@@ -17,34 +16,74 @@ namespace alex::utils::math::ct_power
 {
 namespace concepts
 {
-
+#if __cplusplus > 201703L && __cpp_concepts >= 201907L
 template<typename T>
-concept NumberType = requires
-{ requires std::is_integral_v<T> || std::is_floating_point_v<T> || boost::multiprecision::is_number<T>::value; };
-
+concept NumberType = std::is_integral_v<T> ||
+                    std::is_floating_point_v<T> ||
+                    boost::multiprecision::is_number<T>::value;
+#else
+template<typename T> struct IsNumberType
+{ static constexpr bool value{std::is_integral<T>::value || std::is_floating_point<T>::value}; };
+#endif
 } // namespace alex::utils::math::ct_power::concepts
 
 
 
-#if 0
-template<concepts::NumberType number_type, number_type number, size_t pow>
+// Recursive template instantiation
+#if 1
+template<
+#if __cplusplus > 201703L && __cpp_concepts >= 201907L
+    concepts::NumberType number_type,
+    number_type number,
+    size_t pow
+#else
+    // Legacy SFINAE
+    typename number_type,
+    number_type number,
+    size_t pow,
+    typename = std::enable_if_t<concepts::IsNumberType<number_type>::value>
+#endif
+>
 struct ct_power
 { static constexpr number_type value{number * ct_power<number_type, number, pow - 1>::value}; };
 
-template<concepts::NumberType number_type, number_type number>
-struct ct_power<number_type, number, 1> { static constexpr number_type value{number}; };
-
-template<concepts::NumberType number_type, number_type number>
-struct ct_power<number_type, number, 0> { static constexpr number_type value{static_cast<number_type>(1)}; };
+template<
+#if __cplusplus > 201703L && __cpp_concepts >= 201907L
+    concepts::NumberType number_type,
+    number_type number
 #else
-template<concepts::NumberType number_type>
+    // Legacy SFINAE
+    typename number_type,
+    number_type number
+#endif
+> struct ct_power<number_type, number, 1> { static constexpr number_type value{number}; };
+
+template<
+#if __cplusplus > 201703L && __cpp_concepts >= 201907L
+    concepts::NumberType number_type,
+    number_type number
+#else
+    // Legacy SFINAE
+    typename number_type,
+    number_type number
+#endif
+> struct ct_power<number_type, number, 0> { static constexpr number_type value{static_cast<number_type>(1)}; };
+#else
+template<
+#if __cplusplus > 201703L && __cpp_concepts >= 201907L
+    concepts::NumberType number_type
+#else
+    // Legacy SFINAE
+    typename number_type, typename = std::enable_if_t<concepts::IsNumberType<number_type>::value>
+#endif
+>
 constexpr number_type ct_power(number_type number, size_t pow)
 {
     if (pow == 0) return 1;
     if (pow == 1) return number;
     return number * ct_power(number, pow - 1);
 }
-} // namespace alex::utils::math::ct_power
 #endif
 
+} // namespace alex::utils::math::ct_power
 #endif // COMPILE_TIME_POWER_HPP
