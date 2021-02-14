1dfae472a5c6502e59e5d3563c07b43b77b484ed
AlexCr4ckPentest <alexandr-119@mail.ru>
2021-02-14 11:01:07 +0300

    fix perfect forwarding

--- include/utils/transparent_invoke.hpp
+++ include/utils/transparent_invoke.hpp
@@ -7,14 +7,13 @@
 #ifndef TRANSPARENT_INVOKE_HPP
 #define TRANSPARENT_INVOKE_HPP
 
-namespace alex::utils {
-
+namespace alex::utils
+{
 template<typename Callable, typename... Args>
 decltype(auto) transparent_invoke(Callable&& callable, Args&&... args)
 {
-    return callable(std::forward<Args&&>(args)...);
+    return callable(std::forward<Args>(args)...);
 }
-
 } // namespace alex::utils
 
 #endif // TRANSPARENT_INVOKE_HPP
