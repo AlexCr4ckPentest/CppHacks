7d503d16a93d3e395883590e93387ee023967910
AlexCr4ckPentest <alexandr-119@mail.ru>
2021-02-20 15:28:32 +0300

    reworked network module and add new tcp_server

--- include/utils/network/network.hpp
+++ include/utils/network/network.hpp
@@ -1,9 +1,6 @@
 #ifndef NETWORK_HPP
 #define NETWORK_HPP
 
-#include "defines_and_constants.hpp"
-
 #include "tcp_server.hpp"
-#include "tcp_client.hpp"
 
 #endif // NETWORK_HPP
--- include/utils/network/tcp_server.hpp
+++ include/utils/network/tcp_server.hpp
@@ -1,21 +1,15 @@
 /* tcp_server.hpp
  *
- * Simple TCP server
+ * Simple TCP server, based on Boost.Asio library
 */
 
 
 
-#include <cstdint>
-#include <array>
-#include <string>
+#include <memory>
 #include <functional>
-#include <stdexcept>
-#if 0
-#include <thread>
-#include <mutex>
-#endif
 
-#include <netinet/in.h>
+#include <boost/asio/io_context.hpp>
+#include <boost/asio/ip/tcp.hpp>
 
 
 
@@ -24,92 +18,165 @@
 
 namespace alex::utils::network
 {
-namespace detail
-{
-constexpr static inline uint16_t BUFFER_SIZE {2048}; // 2 Kib
+namespace asio = boost::asio;
 
-template<typename T>
-concept MessageHandler = std::is_invocable_r_v<void, T, std::string_view>;
-} // namespace alex::utils::network::detail
-
-namespace errors
-{
-struct ServerError : public std::exception
-{
-    explicit ServerError(const char* message)
-        : message_{message}
-    {}
-
-    const char* what() const noexcept override
-    { return message_; }
-
-private:
-    const char* message_;
-};
-} // namespace alex::utils::network::errors
-
-
-
-class TcpServer
+class tcp_session : public std::enable_shared_from_this<tcp_session>
 {
 public:
-    explicit TcpServer(uint16_t port, std::string_view ip_address = ""); // Empty string == INADDR_ANY (listen everithing on port port_)
-    TcpServer(const sockaddr_in& addr_info);
+  explicit tcp_session(asio::ip::tcp::socket&& socket)
+    : socket_{std::move(socket)}
+  {}
 
-    ~TcpServer();
+  ~tcp_session()
+  { when_destryed_(); }
 
-    // No copy, no move
-    TcpServer(TcpServer&& other)                    = delete;
-    TcpServer(const TcpServer& other)               = delete;
-    TcpServer& operator=(TcpServer&& other)         = delete;
-    TcpServer& operator=(const TcpServer& other)    = delete;
+  // No copy, no move
+  tcp_session(tcp_session&&) = delete;
+  tcp_session(const tcp_session&) = delete;
+  tcp_session& operator=(tcp_session&&) = delete;
+  tcp_session& operator=(const tcp_session&) = delete;
 
-    void start(uint16_t max_clients = SOMAXCONN);
-    void stop();
 
-    inline uint16_t get_port() const noexcept
-    { return port_; }
 
-    inline std::string get_ip_address() const noexcept
-    { return ip_address_; }
+  virtual void start() &
+  { receive(); }
 
-    inline sockaddr_in get_client_info() const noexcept
-    { return client_addr_info_; }
+  tcp_session* when_destroyed(const std::function<void()>& handler) &
+  {
+    when_destryed_ = handler;
+    return this;
+  }
 
-#if 0
-    void set_port(uint16_t port) noexcept
-    { port_ = port; }
-
-    void set_ip_address(std::string_view ip_address)
-    { ip_address_ = ip_address; }
-#endif
-
-    template<detail::MessageHandler message_handler_t>
-    inline void install_message_handler(message_handler_t message_handler)
-    { message_handler_ = message_handler; }
+  tcp_session* when_data_received(const std::function<void(std::string_view)>& handler) &
+  {
+    when_data_received_ = handler;
+    return this;
+  }
 
 protected:
-    const uint16_t port_;
-    std::string ip_address_;
+  virtual void receive()
+  {
+    socket_.async_read_some(asio::buffer(buffer_, buffer_.max_size()),
+                            [self = shared_from_this(), this](boost::system::error_code error, size_t n)
+    {
+      if (!error)
+      {
+#ifdef _TCP_ECHO_SERVER_
+        send(n);
+#else
+        receive();
+#endif
+        when_data_received_({buffer_.data(), n});
+      }
+    });
+  }
 
-    int server_socket_fd_;
-    int client_socket_fd_;
-    sockaddr_in server_addr_info_;
-    sockaddr_in client_addr_info_;
-
-    virtual void listen(uint16_t max_clients);
-    virtual ssize_t receive() const noexcept;
+#ifdef _TCP_ECHO_SERVER_
+  virtual void send(size_t n)
+  {
+    socket_.async_write_some(asio::buffer(buffer_, n),
+                             [self = shared_from_this(), this](boost::system::error_code error, size_t)
+    {
+      if (!error)
+      {
+        receive();
+      }
+    });
+  }
+#endif
 
 private:
-    static std::function<void(std::string_view)> message_handler_;
-    static std::array<char, detail::BUFFER_SIZE> buffer_;
+  asio::ip::tcp::socket socket_;
+
+  static std::array<char, 2048> buffer_;
+  static std::function<void()> when_destryed_;
+  static std::function<void(std::string_view)> when_data_received_;
 };
 
+/* Initializing static members for class tcp_session
+ */
+std::array<char, 2048> tcp_session::buffer_{};
+std::function<void()> tcp_session::when_destryed_{[]() {}};
+std::function<void(std::string_view)> tcp_session::when_data_received_{[](std::string_view) {}};
+
+
+
+class tcp_server
+{
+public:
+  tcp_server(asio::io_context& io_con, const asio::ip::tcp::endpoint& endpoint)
+    : server_endpoint_{endpoint}
+    , tcp_acceptor_{io_con, server_endpoint_}
+  {}
+
+  tcp_server(asio::io_context& io_con, uint16_t port)
+    : server_endpoint_{boost::asio::ip::tcp::v4(), port}
+    , tcp_acceptor_{io_con, server_endpoint_}
+  {}
+
+  // No copy, no move
+  tcp_server(tcp_server&&) = delete;
+  tcp_server(const tcp_server&) = delete;
+  tcp_server& operator=(tcp_server&&) = delete;
+  tcp_server& operator=(const tcp_server&) = delete;
+
+
+
+  virtual void run() &
+  {
+    tcp_acceptor_.async_accept([this](boost::system::error_code error, asio::ip::tcp::socket&& socket)
+    {
+      if (!error)
+      {
+        client_endpoint_ = socket.remote_endpoint();
+        client_connected_handler_();
+
+        std::make_shared<tcp_session>(std::move(socket))
+            ->when_destroyed(client_disconnected_handler_)
+            ->when_data_received(data_received_handler_)
+            ->start();
+      }
+    });
+  }
+
+  asio::ip::tcp::endpoint remote_endpoint() const noexcept
+  { return client_endpoint_; }
+
+  tcp_server& when_client_connected(const std::function<void()>& client_connected_handler)
+  {
+    client_connected_handler_ = client_connected_handler;
+    return *this;
+  }
+
+  tcp_server& when_client_disconnected(const std::function<void()>&& client_disconnected_handler)
+  {
+    client_disconnected_handler_ = client_disconnected_handler;
+    return *this;
+  }
+
+  tcp_server& when_data_received(const std::function<void(std::string_view)>& data_received_handler)
+  {
+    data_received_handler_ = data_received_handler;
+    return *this;
+  }
+
+private:
+  asio::ip::tcp::endpoint server_endpoint_;
+  asio::ip::tcp::acceptor tcp_acceptor_;
+
+  asio::ip::tcp::endpoint client_endpoint_;
+
+  static std::function<void()> client_connected_handler_;
+  static std::function<void()> client_disconnected_handler_;
+  static std::function<void(std::string_view)> data_received_handler_;
+};
+
+/* Initializing static members for class tcp_server
+ */
+std::function<void()> tcp_server::client_connected_handler_{[]() {}};
+std::function<void()> tcp_server::client_disconnected_handler_{[]() {}};
+std::function<void(std::string_view)> tcp_server::data_received_handler_{[](std::string_view) {}};
+
 } // namespace alex::utils::network
-
-
-
-// Implementation here
-#include "impl/tcp_server/tcp_server_impl.hpp"
 
 #endif // TCP_SERVER_HPP
