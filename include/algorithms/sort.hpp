e02409426f28937dea7a29cf73929fa63d22b93a
AlexCr4ckPentest <alexandr-119@mail.ru>
2021-03-13 15:32:17 +0300

    add bubble_sort() implementation

--- include/algorithms/sort.hpp
+++ include/algorithms/sort.hpp
@@ -16,9 +16,44 @@
 
 namespace alex::algorithms::sort
 {
-  template<std::random_access_iterator Iterator, typename Comparator = std::less<>>
+  namespace detail
+  {
+    template<typename T>
+    concept RandomAccessIterable = requires (T t)
+    {
+      { t.begin() }   -> std::random_access_iterator;
+      { t.end() }     -> std::random_access_iterator;
+      { t.cbegin() }  -> std::random_access_iterator;
+      { t.cend() }    -> std::random_access_iterator;
+      { t.rbegin() }  -> std::random_access_iterator;
+      { t.rend() }    -> std::random_access_iterator;
+      { t.crbegin() } -> std::random_access_iterator;
+      { t.crend() }   -> std::random_access_iterator;
+    };
+  } // namesapce alex::algorithms::sort::detail
+
+  // Overload: bubble_sort(begin, end)
+  template<std::random_access_iterator Iterator, typename Comparator = std::greater<>>
   void bubble_sort(Iterator first, Iterator last, Comparator&& comp = Comparator{})
-  { /* Implementation here */ }
+  {
+    const auto diff{std::distance(first, last)};
+
+    for (auto i{0}; i < diff; i++)
+    {
+      for (Iterator it{first}; it != (last - i - 1); ++it)
+      {
+        if (comp(*it, *std::next(it)))
+        {
+          std::swap(*it, *std::next(it));
+        }
+      }
+    }
+  }
+
+  // Overload: bubble_sort(collection)
+  template<detail::RandomAccessIterable Collection, typename Comparator = std::greater<>>
+  void bubble_sort(Collection&& collection, Comparator&& comp = Comparator{})
+  { bubble_sort(std::begin(collection), std::end(collection), comp); }
 
   template<std::random_access_iterator Iterator, typename Comparator = std::less<>>
   void quick_sort(Iterator first, Iterator last, Comparator&& comp = Comparator{})
